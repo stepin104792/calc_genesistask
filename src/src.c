63b1fcbfc37b85a7319d4174074a7aaca4b8e26c
stepin104792 <69047841+stepin104792@users.noreply.github.com>
2020-09-19 11:20:17 +0530

    Update src.c

--- src/src.c
+++ src/src.c
@@ -1,1 +1,38 @@
 
+#include <calculator_operations.h>
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
+int factorial(int operand1)
+{
+  /* Return -1 for negative numbers */
+  if(operand1 < 0)
+    return -1;
+
+  /* Return 1 for 0 */
+  if(operand1 == 0)
+    return 1;
+
+  /* Recursively calculate Factorial of the number */
+  return operand1 * factorial(operand1-1);
+}
